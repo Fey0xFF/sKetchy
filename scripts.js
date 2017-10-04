646f917920571c8196c4e53bc8937b2d7135a1b1
Anthony Bendas <anthony.bendas@gmail.com>
2017-10-03 19:52:06 -0600

    added grid resizing functions and custom settings

--- scripts.js
+++ scripts.js
@@ -5,21 +5,52 @@ function gridInit (numSquare,gridHeight,gridWidth) {
     for (cols = 0; cols < numSquare; cols++) {
       $("#container").append("<div class = 'square'></div>");
     }
-    $('.square').width(gridWidth/numSquare);
-    $('.square').height(gridHeight/numSquare);
   }
+  $('.square').width(gridWidth/numSquare);
+  $('.square').height(gridHeight/numSquare);
 }
 
+
+
+function resetGrid () {
+  $(".square").remove();
+}
+
+function userDefineGrid () {
+  resetGrid();
+  var uGW = $('#userGridWidth').val();
+  var uGH = $('#userGridHeight').val();
+  var uGU = $('#userGridUnits').val();
+  gridInit(uGU,uGW,uGH);
+}
+
+
 $(document).ready(function() {
-  gridInit(5,500,500);
+  gridInit(16,500,500);
 
-  $(".square").mouseenter(function() {
-    console.log("mouseover");
-    $(this).addClass("mouseOn");
-  });
+  $("#resetGrid").click(function (){
+    resetGrid();
+    gridInit(5,500,500);
 
-  $(".square").mouseleave(function() {
+    $(".square").mouseenter(function() {
+      console.log("mouseover");
+      $(this).addClass("mouseOn");
+    });
+  })
+
+  $("#userResetGrid").click(function (){
+    userDefineGrid();
+
+    $(".square").mouseenter(function() {
+      console.log("mouseover");
+      $(this).addClass("mouseOn");
+    });
+  })
+
+
+
+/*  $(".square").mouseleave(function() {
     console.log("mouseover");
     $(this).removeClass("mouseOn");
-  });
+  });*/
 });
